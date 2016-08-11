5cb1f6d3aba2fc3618654b8a548cca9e79836ffa
Chuabacca <jonathan.chua@dollarshaveclub.com>
2016-08-11 09:50:16 -0700

    Installed ember-router-scroll.

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'router-scroll-demo',
     environment: environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'router-scroll',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
